00438ea88bf7d2fc99496aaed5db139a84c1cecf
IvanNarkevich <47961410+IvanNarkevich@users.noreply.github.com>
2019-10-14 14:06:41 +0300

    Add files via upload

--- Hospital/src/com/narkevich/bsuirlab/Hospital.java
+++ Hospital/src/com/narkevich/bsuirlab/Hospital.java
@@ -1,29 +1,31 @@
 package com.narkevich.bsuirlab;
 
+import java.util.List;
+
 public class Hospital {
     private static Hospital ourInstance;
     private String country;
     private String city;
     private String name;
+    private String address;
+    private List<Department> departments;
 
-    public static Hospital getInstance(String country, String city, String name) {
+    public static Hospital getInstance(String country, String city, String name, String address) {
         if (ourInstance == null) {
-            ourInstance = new Hospital(country, city, name);
+            ourInstance = new Hospital(country, city, name, address);
         }
         return ourInstance;
     }
 
-    private Hospital(String country, String city, String name) {
+    private Hospital(String country, String city, String name, String address) {
         this.country = country;
         this.city = city;
         this.name = name;
+        this.address = address;
     }
 
-    public void addDoctor(Doctor doctor) {
+    public void addDepartment() {
 
     }
 
-    public void addPatient(Patient patient) {
-
-    }
 }
