d1051a6fe0d61c33131b319e29d91fadb47e56d9
IvanNarkevich <vanyanarkevich@gmail.com>
2019-11-16 22:31:00 +0300

    deleting

--- Hospital/src/com/narkevich/bsuirlab/model/service/DepartmentAndDoctorMismatchException.java
+++
@@ -1,7 +0,0 @@
-package com.narkevich.bsuirlab.model.service;
-
-public class DepartmentAndDoctorMismatchException extends Exception {
-    public DepartmentAndDoctorMismatchException(){
-
-    }
-}
